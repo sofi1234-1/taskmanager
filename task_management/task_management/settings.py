9e2146e0fc42c235866346f6c34e4c5941d8c520
Ermi539 <ermiashabtamu477@gmail.com>
2024-10-16 02:50:38 -0900

    Update settings.py

--- task_management/task_management/settings.py
+++ task_management/task_management/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-wt5d#naua6jriqog35%4#tkt+lk0j8y_=ozipqz1)p)j(g_ntu
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['task_management.railway.app']
+ALLOWED_HOSTS = ['task-manager-final-project-ten.vercel.app']
 
 
 # Application definition
