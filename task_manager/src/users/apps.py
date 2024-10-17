2c35aa5bfd3ee72720b7628bf38b792c7e998542
sofi1234-1 <sofoniasalemu15@gmail.com>
2024-10-17 20:35:49 +0300

    Delete task_manager directory

--- task_manager/src/users/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-from django.contrib.auth.models import AbstractUser
-
-class UsersConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'users'
