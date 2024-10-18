b29cd68dd2e62b9cdc5fd83be2b20ed439204d60
sofi1234-1 <sofoniasalemu15@gmail.com>
2024-10-18 10:02:07 +0300

    Update serializers.py

--- tasks/serializers.py
+++ tasks/serializers.py
@@ -1,25 +1,21 @@
-<<<<<<< HEAD
 from rest_framework import serializers, viewsets
 from .models import Task
 from django.utils import timezone 
-=======
-from rest_framework import serializers
-from .models import Task
 from django.contrib.auth.models import User
-from django.utils import timezone
 
->>>>>>> origin/main
+
+
 class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
         fields = '__all__'
-<<<<<<< HEAD
+
 
     def validate_due_date(self, value):
         if value < timezone.now().date():
             raise serializers.ValidationError("Due date must be in the future.")
         return value
-=======
+
         extra_kwargs = {
             'due_date': {'required': True},
         }
@@ -41,4 +37,4 @@ class UserSerializer(serializers.ModelSerializer):
         user.set_password(password)
         user.save()
         return user
->>>>>>> origin/main
+
