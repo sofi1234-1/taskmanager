155ad8d92aab8869c716783b0be5a394f892326f
sofi1234-1 <sofoniasalemu15@gmail.com>
2024-10-19 10:54:16 +0300

    Update settings.py

--- task_management/settings.py
+++ task_management/settings.py
@@ -14,13 +14,8 @@ import dj_database_url
 from pathlib import Path
 
 
-
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-wt5d#naua6jriqog35%4#tkt+lk0j8y_=ozipqz1)p)j(g_ntu'
